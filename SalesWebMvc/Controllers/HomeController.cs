46ba35aa76b6e43e22975ea97ed2789f71a1e0ce
joao96 <jvpoletti@gmail.com>
2020-12-01 14:27:35 -0300

    Added Department Controller and View

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC App with C#";
+            // will search in the folder Views, inside subfolder Home (because this action belongs to the HomeController), a page called About
             return View();
         }
 
